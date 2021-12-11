2fd9161350d439cf6b7d4bd309acf465f65ab16f
Ajay Chauhan <ajay@logiqids.com>
2021-12-11 14:13:26 +0530

    initial commit

---
+++ src/routers/user.js
@@ -0,0 +1,100 @@
+const express = require("express")
+const auth = require("../middleware/auth")
+const { dbQuery } = require("../common/utils")
+const router = new express.Router()
+
+router.post("/user/login", async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+    if (!(email && password))
+        res.status(401).send("Please provide Email and Password")
+    let results = await dbQuery(
+        `SELECT * FROM users WHERE email='${email}' AND password='${password}';`
+    )
+    if (!results.length) res.status(404).send({ error: "Unauthorized" })
+    user = results[0]
+    data = {
+        userId: user.id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone
+    }
+    res.send(data)
+})
+
+router.get("/user/:userId/profile", auth("userId"), async (req, res) => {
+    data = {
+        userId: req.user.id,
+        name: req.user.name,
+        email: req.user.email,
+        phone: req.user.phone
+    }
+    res.send(data)
+})
+
+router.post(
+    "/user/:userId/profile-update",
+    auth("userId"),
+    async (req, res) => {
+        try {
+            query = `UPDATE users SET name='${req.body.name}', email='${req.body.email}', phone='${req.body.phone}' WHERE id=${req.user.id};`
+            await dbQuery(query)
+            data = req.body
+            data.userId = req.user.id
+            res.send(req.body)
+        } catch (e) {
+            res.status(400).send("Update Failed")
+        }
+    }
+)
+
+router.post(
+    "/user/:userId/get-total-amount",
+    auth("userId"),
+    async (req, res) => {
+        const fromDate = req.body.fromDate
+        const toDate = req.body.toDate
+        if (!(fromDate && toDate))
+            res.status(400).send("Please provide Date Range")
+        results = await dbQuery(
+            `select sum(case when sender_id = ${req.user.id} then amount else null end) as amount_sent, sum(case when receiver_id = ${req.user.id} then amount else null end) as amount_received from transactions where sender_id = ${req.user.id} or receiver_id = ${req.user.id} and date(transaction_date) >= '${fromDate}' and date(transaction_date) <= '${toDate}';`
+        )
+        if (!results.length) res.send("No Data Found")
+        data = results[0]
+        res.send(data)
+    }
+)
+
+router.get("/get-best-users", async (req, res) => {
+    results = await dbQuery(
+        `SELECT u.name, SUM(t.amount) AS total_transactions_amount FROM users u LEFT JOIN transactions t ON (u.id = t.sender_id OR u.id = t.receiver_id) group by u.id ORDER BY SUM(t.amount) DESC LIMIT 3;`
+    )
+    res.send(results)
+})
+
+router.post(
+    "/user/:userId/amount-per-month",
+    auth("userId"),
+    async (req, res) => {
+        const month = req.body.month
+        if (!month) res.status(400).send("Please provide Month")
+        results = await dbQuery(
+            `select sum(case when t.sender_id = ${req.user.id} then t.amount else null end) as total_amount_sent, sum(case when t.receiver_id = ${req.user.id} then t.amount else null end) as total_amount_received, avg(case when t.sender_id = ${req.user.id} then t.amount else null end) as average_amount_sent, avg(case when t.receiver_id = ${req.user.id} then t.amount else null end) as average_amount_received from transactions t where t.sender_id = ${req.user.id} or t.receiver_id = ${req.user.id} and month(t.transaction_date) = ${month};`
+        )
+        if (!results.length) res.send("No Data Found")
+        data = results[0]
+        res.send(data)
+    }
+)
+
+router.post("/max-amount-for-month", async (req, res) => {
+    const month = req.body.month
+    if (!month) res.status(400).send("Please provide Month")
+    results = await dbQuery(
+        `select t.id as transaction_id, s.name as sender, r.name as receiver, t.amount, t.transaction_date, t.transaction_type from transactions t, users s, users r where t.sender_id = s.id and t.receiver_id = r.id and amount = (select max(amount) from transactions where month(transaction_date) = ${month});`
+    )
+    if (!results.length) res.send("No Data Found")
+    res.send(results)
+})
+
+module.exports = router
