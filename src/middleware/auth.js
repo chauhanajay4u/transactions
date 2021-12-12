35d29e3403ebe6d144a72fc0b38e0906625f032c
Ajay Chauhan <ajay@logiqids.com>
2021-12-12 13:04:02 +0530

    return fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,10 @@ const auth = (roleKey) => {
         var userId = req.params[roleKey]
         req.userId = userId
         let data = await dbQuery(`select * from users where id = ${req.userId}`)
-        if (!data.length) res.status(404).send("User not found")
+        if (!data.length) {
+            res.status(404).send("User not found")
+            return True
+        }
         req.user = data[0]
         next()
     }
